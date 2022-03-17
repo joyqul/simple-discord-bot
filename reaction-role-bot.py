81056731c58e8bb626492dc4c05c0844f7f196e6
joyqul <joyqul.tw@gmail.com>
2022-03-17 19:52:22 +0800

    add reaction-role-bot

---
+++ reaction-role-bot.py
@@ -0,0 +1,32 @@
+import discord
+import os
+
+intents = discord.Intents(messages=True,
+                          guilds=True,
+                          reactions=True,
+                          members=True)
+client = discord.Client(intents=intents)
+
+
+def get_member_and_role_from_payload(payload):
+    guild_id = payload.guild_id
+    guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+    role = discord.utils.get(guild.roles, name=payload.emoji.name)
+    member = discord.utils.find(lambda m: m.id == payload.user_id,
+                                guild.members)
+    return member, role
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    member, role = get_member_and_role_from_payload(payload)
+    await member.add_roles(role)
+
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    member, role = get_member_and_role_from_payload(payload)
+    await member.remove_roles(role)
+
+
+client.run(os.getenv('TOKEN'))
